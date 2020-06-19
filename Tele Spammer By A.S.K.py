da4a6045fc4b62d7161a1e8f7e93a7ea53a4944d
ask3 <42865251+A-S-K-3@users.noreply.github.com>
2020-06-19 16:13:14 +0530

    Initial Commit

---
+++ Tele Spammer By A.S.K.py
@@ -0,0 +1,48 @@
+from telethon.sync import TelegramClient
+from telethon.tl.functions.messages import GetDialogsRequest
+from telethon.tl.types import InputPeerEmpty, InputPeerChannel, InputPeerUser
+from telethon.errors.rpcerrorlist import PeerFloodError, UserPrivacyRestrictedError
+from telethon.tl.functions.channels import InviteToChannelRequest
+import sys
+import traceback
+import time
+import random
+import re
+api_id = 000000  # API ID
+api_hash = ''  #API HASH
+phone = '+'  #NUMBER WITH COUNTRY CODE
+timer = 10 #TIME TO WAIT BEFORE NEXT SENDING
+msgtosend = "Hello World" # MESSAGE TO SEND
+client = TelegramClient(phone, api_id, api_hash)
+client.connect()
+if not client.is_user_authorized():
+    client.send_code_request(phone)
+    client.sign_in(phone, input('Enter the code: '))
+chats = []
+last_date = None
+chunk_size = 10
+groups=[]
+result = client(GetDialogsRequest(
+                offset_date=last_date,
+                offset_id=0,
+                offset_peer=InputPeerEmpty(),
+                limit=chunk_size,
+                hash = 0
+            ))
+chats.extend(result.chats)
+for chat in chats:
+    try:
+        groups.append(chat)
+    except:
+        continue
+while True:    
+    for group in groups:
+        try:
+            username1 = group.username
+            print(username1)    
+            client.send_message(username1, msgtosend)
+            time.sleep(1)
+        except:
+            continue 
+    print("Send Complete!!, Waiting for " + str(timer) + " seconds")
+    time.sleep(timer)
